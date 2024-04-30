fe35f8046b8f54b6bab739f995c390f519d2b32b
Domenico Luciani <domenicoleoneluciani@gmail.com>
2024-04-30 13:35:42 +0200

    Fix two typos

--- network/client_test.go
+++ network/client_test.go
@@ -8,10 +8,10 @@ import (
 )
 
 func Test(t *testing.T) {
-	t.Run("Shoukld check if the response starts with the same ID as the query", func(t *testing.T) {
+	t.Run("Should check if the response starts with the same ID as the query", func(t *testing.T) {
 		query, _ := hex.DecodeString("00160100000100000000000003646e7306676f6f676c6503636f6d0000010001")
 		response, _ := hex.DecodeString("00168080000100020000000003646e7306676f6f676c6503636f6d0000010001c00c0001000100000214000408080808c00c0001000100000214000408080404")
 
-		assert.True(t, hasTheSsameID(query, response))
+		assert.True(t, hasTheSameID(query, response))
 	})
 }
