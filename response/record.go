253adeffd8b6b25925d13a24a9bff7cd3ef32f47
Domenico Luciani <domenicoleoneluciani@gmail.com>
2024-05-01 00:08:04 +0200

    Improve readability of the decodeName function

--- response/record.go
+++ response/record.go
@@ -10,46 +10,17 @@ import (
 
 func ParseRecord(reader *bytes.Reader) *packet.Record {
 	var record packet.Record
-
 	record.Name = []byte(decodeName(reader))
-
-	var recordType uint16
-	if err := binary.Read(reader, binary.BigEndian, &recordType); err != nil {
-		fmt.Printf("ERRRR: %v", err)
-		return nil
-	}
-	record.Type = recordType
-
-	var recordClass uint16
-	if err := binary.Read(reader, binary.BigEndian, &recordClass); err != nil {
-		fmt.Printf("ERRRR: %v", err)
-
-		return nil
-	}
-	record.Class = recordClass
-
-	if err := binary.Read(reader, binary.BigEndian, &record.TTL); err != nil {
-		fmt.Printf("ERRRR: %v", err)
-
-		return nil
-	}
-
-	var rdLength uint16
-	if err := binary.Read(reader, binary.BigEndian, &rdLength); err != nil {
-		fmt.Printf("ERRRR: %v", err)
-
-		return nil
-	}
-	record.RdLength = rdLength
-
-	dataBytes := make([]byte, record.RdLength)
-	if err := binary.Read(reader, binary.BigEndian, &dataBytes); err != nil {
-		fmt.Printf("ERRRR: %v", err)
-
-		return nil
-	}
-
-	record.Rdata = fmt.Sprintf("%d.%d.%d.%d", dataBytes[0], dataBytes[1], dataBytes[2], dataBytes[3])
-
+	binary.Read(reader, binary.BigEndian, &record.Type)
+	binary.Read(reader, binary.BigEndian, &record.Class)
+	binary.Read(reader, binary.BigEndian, &record.TTL)
+	binary.Read(reader, binary.BigEndian, &record.RdLength)
+	record.Rdata = readData(reader, record.RdLength)
 	return &record
+}
+
+func readData(reader *bytes.Reader, length uint16) string {
+	dataBytes := make([]byte, length)
+	binary.Read(reader, binary.BigEndian, &dataBytes)
+	return fmt.Sprintf("%d.%d.%d.%d", dataBytes[0], dataBytes[1], dataBytes[2], dataBytes[3])
 }
--- response/response.go
+++ response/response.go
@@ -2,7 +2,6 @@ package response
 
 import (
 	"bytes"
-	"fmt"
 	"io"
 )
 
@@ -13,70 +12,36 @@ func decodeName(reader *bytes.Reader) string {
 		lengthByte, _ := reader.ReadByte()
 
 		if (lengthByte & 0xC0) == 0xC0 {
-
-			nextByte, err := reader.ReadByte()
-			if err != nil {
-				fmt.Printf("ERRRR: %v", err)
-				break
-			}
-
-			pointer := uint16((uint16(lengthByte) & 0x3F) | uint16(nextByte))
-
-			currentPos, err := reader.Seek(0, io.SeekCurrent)
-			if err != nil {
-				fmt.Printf("ERRRR: %v", err)
-				break
-			}
-
-			_, err = reader.Seek(int64(pointer), io.SeekStart)
-			if err != nil {
-				fmt.Printf("ERRRR: %v", err)
-				break
-			}
-
-			decodedName := decodeName(reader)
-			_, err = name.WriteString(decodedName)
-			if err != nil {
-				fmt.Printf("ERRRR: %v", err)
-				break
-			}
-
-			_, err = reader.Seek(currentPos, io.SeekStart)
-			if err != nil {
-				fmt.Printf("ERRRR: %v", err)
-				break
-			}
-
+			name.WriteString(getBackTheDomainFromTheHeader(reader, lengthByte))
 			break
-		} else {
-			label := make([]byte, lengthByte)
-			_, err := reader.Read(label)
-			if err != nil {
-				fmt.Printf("ERRRR: %v", err)
-				break
-			}
-			_, err = name.Write(label)
-			if err != nil {
-				fmt.Printf("ERRRR: %v", err)
-				break
-			}
-
-			if lengthByte == 0 {
-				break
-			}
-
-			_, err = name.WriteString(".")
-			if err != nil {
-				fmt.Printf("ERRRR: %v", err)
-				break
-			}
 		}
+
+		if lengthByte == 0 {
+			break
+		}
+
+		label := make([]byte, lengthByte)
+		io.ReadFull(reader, label)
+		if name.Len() > 0 {
+			name.WriteByte('.')
+		}
+		name.Write(label)
 	}
 
-	result := name.String()
-	if len(result) > 0 && result[len(result)-1] == '.' {
-		result = result[:len(result)-1]
-	}
+	return name.String()
+}
 
-	return result
+func getBackTheDomainFromTheHeader(reader *bytes.Reader, lengthByte byte) string {
+	nextByte, _ := reader.ReadByte()
+	pointer := uint16((uint16(lengthByte) & 0x3F) | uint16(nextByte))
+
+	currentPos, _ := reader.Seek(0, io.SeekCurrent)
+
+	reader.Seek(int64(pointer), io.SeekStart)
+
+	decodedName := decodeName(reader)
+
+	reader.Seek(currentPos, io.SeekStart)
+
+	return decodedName
 }
